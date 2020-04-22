f0fb3769e3c598363fd2f5c40e6386c2bfafa41b
Mateus Fiori <matheus.fiore87@gmail.com>
2020-04-21 21:47:09 -0300

    App My-Money-App em Construção

---
+++ my-money-app/backend/src/config/server.js
@@ -0,0 +1,15 @@
+const bodyParser = require('body-parser');
+const express = require('express');
+
+const port = 3000;
+
+const server = express();
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, function () {
+    console.log(`BACKEND is running on port ${port}.`);
+})
+
+module.exports = server;
